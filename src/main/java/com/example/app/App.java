445c75e3353b3079b1d2ceabfdae29db727ab6cd
hlavania05 <lavaniaharshita05@gmail.com>
2025-02-14 22:29:48 +0530

    added sonar-project.properties

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -12,6 +12,7 @@ public class App {
                 .filename(".env")
                 .load();
 
+                
         System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
         System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
         System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
