ce6343953eafe92050a27887e446d72e1dd2abf5
hlavania05 <lavaniaharshita05@gmail.com>
2025-04-21 10:23:31 +0530

    checking github webhook

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -21,7 +21,6 @@ public class App {
         System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
         System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
         System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
-
         System.setProperty("aws.s3.bucket-name", dotenv.get("AWS_S3_BUCKET_NAME"));
         System.setProperty("aws.access-key", dotenv.get("AWS_ACCESS_KEY_ID"));
         System.setProperty("aws.secret-key", dotenv.get("AWS_SECRET_ACCESS_KEY"));
